a2cbfb156b2a1d11a43bc95dc124b90af2849f46
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-06-20 14:43:41 -0700

    added filter array

---
+++ FilterArray.js
@@ -0,0 +1,12 @@
+const filterArray = function(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < 5) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
+}
+
+let testArr = [11, 3, 23, 7, 27, 4, 8];
+
+console.log(filterArray(testArr))
